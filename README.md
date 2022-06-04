39aaa436d8558b0573946b1b5ccd7db7e011b2f8
Muhammad Talha <talhawahid886@gmail.com>
2022-06-04 12:43:20 +0500

    CRA & header
    CRA

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 1WB-landing
+
 1WB landing
+
+To run the project:
+
+- `npm install` and `npm start`
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+// Bootstrap link
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Header from "./container/components/header/header";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
