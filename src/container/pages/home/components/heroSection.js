3a48802f2498591459bd0d4581837ea90492cbae
Muhammad Talha <talhawahid886@gmail.com>
2022-06-05 05:08:28 +0500

    Loader Alignment
    Loader Alignment

--- src/container/pages/home/components/heroSection.js
+++ src/container/pages/home/components/heroSection.js
@@ -1,23 +1,9 @@
 import React, { Fragment } from "react";
-import HeroImage from "../../../assets/images/earth-image.png";
 
 export default function heroSection() {
   return (
     <Fragment>
       <div className="container">
-        {/* <div className="row">
-          <div className="video-container">
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/VvokCJFIRK0&t=70?controls=0&autoplay=1&mute=1&showinfo=0&loop=1&playlist=VvokCJFIRK0"
-              title="YouTube video player"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            ></iframe>
-          </div>
-        </div> */}
         <div className="row">
           <div className="d-flex flex-column justify-content-center w-100 h-100"></div>
         </div>
@@ -38,14 +24,7 @@ export default function heroSection() {
           </div>
           <div className="col-md-6 mt-5">
             <div className="img-wrapper">
-              {/* <img
-                src={HeroImage}
-                className="hero-img"
-                effect="blur"
-                alt="Hero Section"
-              /> */}
               <div className="img-wrapper-2">
-                {" "}
                 <div className="img-wrapper-3"></div>
               </div>
             </div>
