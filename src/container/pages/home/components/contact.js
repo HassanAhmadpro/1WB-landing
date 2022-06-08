c50ea3337bc3f26eecabba47271b0b38b982d8ac
Muhammad Talha <talhawahid886@gmail.com>
2022-06-08 22:46:46 +0500

    Contact BG
    Contact BG

--- src/container/pages/home/components/contact.js
+++ src/container/pages/home/components/contact.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import ContactIcon from "../../../assets/images/contact-us.png";
 
 export default function Contact() {
   return (
@@ -7,6 +8,13 @@ export default function Contact() {
         <div class="contact-box">
           <div class="contact-links">
             <h2 className="contact-heading">GET IN TOUCH</h2>
+            <div className="row">
+              <div className="col-lg-8 col-sm-8 col-xs-8 col-8 text-center m-auto">
+                <div className="shadow-contact mt-lg-5 mt-md-4 mt-sm-4 mt-4">
+                  <img src={ContactIcon} className="contact-icon m-auto" />
+                </div>
+              </div>
+            </div>
           </div>
           <div className="contact-form-wrapper">
             <form>
