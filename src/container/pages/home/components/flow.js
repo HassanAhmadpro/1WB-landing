c469feacd1003f46979380a49f0d729af76298fa
Muhammad Talha <talhawahid886@gmail.com>
2022-06-16 23:45:12 +0500

    Images
    Images

--- src/container/pages/home/components/flow.js
+++ src/container/pages/home/components/flow.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
-import MetaMask from "../../../assets/images/metamask.png";
-import Binance from "../../../assets/images/binance.png";
-import Visa from "../../../assets/images/visa.png";
-import MasterCard from "../../../assets/images/mastercard.png";
-import CloudStorage from "../../../assets/images/cloud-storage.png";
-import Computer from "../../../assets/images/computer.png";
-import Database from "../../../assets/images/database.png";
-import Vision from "../../../assets/images/vision.png";
+import ChartImg from "../../../assets/images/chart-img.png";
+import FlowImg from "../../../assets/images/flow-img.png";
+// import MetaMask from "../../../assets/images/metamask.png";
+// import Binance from "../../../assets/images/binance.png";
+// import Visa from "../../../assets/images/visa.png";
+// import MasterCard from "../../../assets/images/mastercard.png";
+// import CloudStorage from "../../../assets/images/cloud-storage.png";
+// import Computer from "../../../assets/images/computer.png";
+// import Database from "../../../assets/images/database.png";
+// import Vision from "../../../assets/images/vision.png";
 import LogoImage from "../../../assets/images/logo.png";
 
 export default function Flow() {
@@ -18,8 +20,9 @@ export default function Flow() {
             <div className="about-main pt-5 pb-5 text-center">
               <h1 className="about-heading">THE IDEAOLOGY</h1>
               <div className="row">
-                <div className="col-lg-5">
-                  <div className="row">
+                <div className="col-lg-5 m-auto">
+                  <img src={ChartImg} className="about-flow-img" />
+                  {/* <div className="row">
                     <div className="col-lg-12 col-md-6 col-sm-6 col-6 text-center">
                       <img src={Binance} className="about-img" />
                     </div>
@@ -32,30 +35,31 @@ export default function Flow() {
                     <div className="col-lg-12 col-md-6 col-sm-6 col-6 mt-3 text-center">
                       <img src={MasterCard} className="about-img" />
                     </div>
-                  </div>
+                  </div> */}
                 </div>
                 <div className="col-lg-2 col-sm-6 col-xs-6 col-8 text-center m-auto">
                   <div className="shadow mt-lg-0 mt-md-4 mt-sm-4 mt-4">
                     <img src={LogoImage} className="about-logo m-auto" />
                   </div>
                 </div>
-                <div className="col-lg-5">
-                  <div className="row mt-5">
+                <div className="col-lg-5 m-auto">
+                  <img src={FlowImg} className="about-chart-img" />
+                  {/* <div className="row mt-5">
                     <div className="col-lg-6 col-sm-5 col-md-5 text-end col-5">
                       <img src={CloudStorage} className="about-img" />
                     </div>
                     <div className="col-lg-6 col-sm-5 text-center col-6">
                       <img src={Computer} className="about-img" />
                     </div>
-                  </div>
-                  <div className="row mt-5">
+                  </div> */}
+                  {/* <div className="row mt-5">
                     <div className="col-lg-6 col-sm-5 col-md-5 text-end mt-3 col-5">
                       <img src={Database} className="about-img" />
                     </div>
                     <div className="col-lg-6 col-sm-5 text-center mt-3 col-6">
                       <img src={Vision} className="about-img" />
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
